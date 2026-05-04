3437516be045d77d86d92feaefb235180722b44e
Angshuman Halder <angshuman.halder243@gmail.com>
2026-05-04 22:27:07 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
+    repl();
+}
+
+fn repl() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+        if input.trim() == "exit" {
+            break;
+        }
         println!("{}: command not found", input.trim());
     }
 }
