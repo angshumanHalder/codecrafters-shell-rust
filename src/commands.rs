43862a2c7efbdf6db91ff5f52467579aea883384
Angshuman Halder <angshuman.halder243@gmail.com>
2026-05-05 12:39:06 +0530

    codecrafters submit [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::process::exit;
 
 pub fn process_command(input: &str) {
@@ -15,6 +16,35 @@ pub fn process_command(input: &str) {
 fn process_type(cmd: &str) {
     match cmd {
         "exit" | "echo" | "type" => println!("{} is a shell builtin", cmd),
-        _ => println!("{}: not found", cmd),
+        _ => match find_command(cmd) {
+            Some(path ) => println!("{} is {}", cmd, path),
+            None => println!("{}: not found", cmd)
+        }
     }
+}
+
+fn find_command(cmd: &str) -> Option<String> {
+    if let Some(path) = std::env::var_os("PATH") {
+        let env_paths = std::env::split_paths(&path);
+        for path in env_paths {
+            let entries = fs::read_dir(path);
+            match entries {
+                Ok(entries) => {
+                    for e in entries {
+                        match e {
+                            Ok(entry) => if let Some(file_name) = entry.path().file_name() {
+                                if file_name == cmd {
+                                    let full_path = entry.path().display().to_string();
+                                    return Some(full_path);
+                                }
+                            },
+                            Err(_) => continue,
+                        }
+                    }
+                }
+                Err(_) => continue
+            }
+        }
+    }
+    None
 }
