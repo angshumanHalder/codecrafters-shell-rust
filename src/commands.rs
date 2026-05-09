6ebcea27623b78455667a06d0a5dc6ca95eb1b14
Angshuman Halder <angshuman.halder243@gmail.com>
2026-05-09 21:00:44 +0530

    codecrafters submit [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -31,7 +31,7 @@ fn process_type(cmd: &str) {
 
 fn find_command(cmd: &str) -> Option<PathBuf> {
     let path = std::env::var_os("PATH").unwrap_or_default();
-    print!("find_command: {}", path.display());
+    println!("find_command: {}", path.display());
     std::env::split_paths(&path)
         .flat_map(|dir| fs::read_dir(dir).ok().into_iter().flatten())
         .filter_map(|e| e.ok())
